3699d237bd9dcb8a74ca9320ab6067b8aa00164b
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-12-03 08:44:30 -0600

    recursive search

--- 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
+++ 02-week-2--recursion/02-day-2--recursive-search/javascript/recursive_search.js
@@ -1,6 +1,22 @@
-function recursiveSearch(arr, target) {
-  // type your code here
+function recursiveSearch(arr, target) { 
+  if(arr[0] === target){
+    return true;
+  } 
+
+  if(arr.length === 0) {
+    return false;
+  }
+  arr.shift()
+  return recursiveSearch(arr, target)
+
+  // if(arr.length > 0) {
+  //   return "true"
+  // } else {
+  //   return "false"
+  // } 
 }
+
+// should probably use nondestructive array methods for these..
 
 if (require.main === module) {
   // add your own tests in here
@@ -15,5 +31,13 @@ if (require.main === module) {
 
 module.exports = recursiveSearch;
 
+
+// UNDERSTAND
+// Create  a recursive method that accepts an array and a target value. if the target value is in the array, the method will return true and vice versa
+
 // Please add your pseudocode to this file
+// init a variable i = 0
+// base case is when i = arr.length OR if arr[i] === target
+// see if arr[i] matches the target value. If it does, return true and stop the method.
+// What is the base case? 
 // And a written explanation of your solution
