23394b45e4413900a97b805eb58e7ec506a7af49
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-12-06 10:28:06 -0600

    fibbonacci recursion

--- 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
+++ 02-week-2--recursion/03-day-3--recursive-fibonacci-series/javascript/fibonacci_recursive.js
@@ -1,5 +1,18 @@
+let arr = [0, 1]
+
 function fibonacci(n) {
-  // type your code here
+  if(n <= 2){
+    return n;
+  };
+
+  if (arr.length === n + 1) {
+    return arr[n]
+  } else {
+    let value = arr[arr.length - 2] + arr[arr.length - 1]
+    arr.push(value)
+    console.log(arr)
+    return fibonacci(n)
+  }  
 }
 
 if (require.main === module) {
@@ -20,5 +33,16 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
+
+// Write a funciton that accepts an integer n as an argument and returns the nth integer of the fibonacci series with recursion (function that calls itself)
+
+
 // Please add your pseudocode to this file
+// 1. Create first base case = if n is less than 2, return n. 
+// 2. initialize an array with first two values of fibbonaci series [0, 1]
+// 3. if n === arr.length return arr[n]
+// 4. else add last two elements of array and push to end. 
+// 5. recall the method. 
+
 // And a written explanation of your solution
+// I created the base case where if n is 1 or 2, it simply returns n. I initialized a fibbonaci array outside of the function. The next base case is where n is equal to the array le
