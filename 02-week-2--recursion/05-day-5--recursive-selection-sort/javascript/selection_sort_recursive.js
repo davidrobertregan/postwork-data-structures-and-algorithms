077a7c03b6b2310c31fa620e518561391bf5b912
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-12-08 10:08:28 -0600

    05-selection-sort-recursive

--- 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
+++ 02-week-2--recursion/05-day-5--recursive-selection-sort/javascript/selection_sort_recursive.js
@@ -1,5 +1,18 @@
 function selectionSortRecursive(arr) {
-  // type your code here
+  if(arr.length === 0){
+    return []
+  }
+
+  let min = Math.min(...arr)
+  let idx = arr.indexOf(min)
+  arr.splice(idx, 1)
+
+  const result = selectionSortRecursive(arr)
+
+  result.unshift(min)
+  return result
+
+  // got stuck on this one :/ but learned a little more
 }
 
 if (require.main === module) {
@@ -7,10 +20,25 @@ if (require.main === module) {
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSortRecursive([3, -1, 5, 2]));
 
+  console.log("Expecting: [-50, 2, 10, 50, 200, 221]");
+  console.log("=>", selectionSortRecursive([10, 221, 50, 200, -50, 2]));
+
+  console.log("Expecting: [10]");
+  console.log("=>", selectionSortRecursive([10]));
+
   console.log("");
 }
 
 module.exports = selectionSortRecursive;
 
+
+// Create a recursive function that accepts an array of numbers and returns a resorted array based on their value
+
+
 // Please add your pseudocode to this file
+// Basecase: array only has one element - then we'll just return that array
+// find the minimum value in array.
+// Create a new array without that value;
+// Recall method with new array
+
 // And a written explanation of your solution
