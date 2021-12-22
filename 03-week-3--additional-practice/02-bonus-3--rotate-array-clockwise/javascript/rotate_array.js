31875f8d2c5bbe2b4b5cc6e78368ae6544366dc0
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-12-22 15:51:12 -0700

    start rotate array

--- 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
+++ 03-week-3--additional-practice/02-bonus-3--rotate-array-clockwise/javascript/rotate_array.js
@@ -1,5 +1,12 @@
 function rotateArray(arr, k) {
-  // type your code here
+  
+  for(const e of arr){
+    if(arr.indexOf(e) >= arr.length - 1){
+      console.log('end')
+    } else {
+      console.log(e)
+    }
+  }
 }
 
 if (require.main === module) {
@@ -7,7 +14,8 @@ if (require.main === module) {
   console.log("Expecting: [4, 1, 2, 3]");
   console.log("=>", rotateArray([1, 2, 3, 4], 1));
 
-  console.log("");
+  console.log("Expecting: [4, 5, 6, 7, 1, 2, 3]");
+  console.log("=>", rotateArray([1, 2, 3, 4, 5, 6, 7], 4));
 
   console.log("Expecting: [2, 3, 1]");
   console.log("=>", rotateArray([1, 2, 3], 2));
@@ -20,5 +28,9 @@ if (require.main === module) {
 
 module.exports = rotateArray;
 
+// create a method that takes an array and a positive integer as an argument that shifts the elements in that array that amount of times
 // Please add your pseudocode to this file
+// I need to adjust the index of each element.... increasing it k number of times... unless that index goes above the array lenght, then we need to loop it back to the front.
+// loop the array and add 
+
 // And a written explanation of your solution
